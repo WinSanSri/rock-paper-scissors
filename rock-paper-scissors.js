74f813ccd51cbd0ae3e6f96a01ce7852dfc8248c
Winnie <winnie.srisok@gmail.com>
2023-06-02 12:37:30 +0700

    Add playRound function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,9 +1,18 @@
+
+//get random choice from computer
 function getComputerChoice() {
+
+    //create array with available choices
     const choices = ['rock', 'paper', 'scissors'];
 
-    let choice;
-
-    choice = choices[Math.floor(Math.random() * 3)];
-    
-    console.log(choice)
+    //get random number from 0-2 (0 included)
+    return choices[Math.floor(Math.random() * 3)];
 }
+
+function playRound(playerSelection, computerSelection) {
+
+    //convert player's choice to lowercase for easier check
+    return playerSelection.toLowerCase() + ' vs. ' + getComputerChoice();
+    
+}
+
