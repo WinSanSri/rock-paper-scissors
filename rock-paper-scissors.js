44daee6c13b912e5617d5877f5e94ba5e3a223d3
Winnie <winnie.srisok@gmail.com>
2023-06-02 15:09:05 +0700

    Fix playRound to get display correct results

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -9,10 +9,48 @@ function getComputerChoice() {
     return choices[Math.floor(Math.random() * 3)];
 }
 
+//play one round
 function playRound(playerSelection, computerSelection) {
 
-    //convert player's choice to lowercase for easier check
-    return playerSelection.toLowerCase() + ' vs. ' + getComputerChoice();
-    
-}
+    //convert to lowercase for easier check
+    playerSelection = playerSelection.toLowerCase();
 
+    //set userWinsRoud to false
+    let userWinsRound = false;
+
+    //display selections
+    console.log('You: ' + playerSelection + ' | ' + 'Computer: ' + computerSelection);
+
+    //user and computer selects same choice
+    if (playerSelection === computerSelection) {
+        return 'It\'s a tie!';
+    }
+
+    //user selected rocks
+    if(playerSelection === 'rock') {
+        if (computerSelection === 'scissors'){
+            userWinsRound = true;
+        }
+    //user selected paper
+    }else if(playerSelection === 'paper') {
+        if (computerSelection === 'rock'){
+            userWinsRound = true;
+        }
+    //user selected scissors
+    }else if(playerSelection === 'scissors') {
+        if (computerSelection === 'paper'){
+            userWinsRound = true;
+
+        }
+    //invalid value
+    }else {
+        return 'invalid'
+    }
+
+    //return the result
+    if (userWinsRound) {
+        return 'You Win! ' + playerSelection +  ' beats ' + computerSelection + '.';
+    }else {
+        return 'You Lose! ' + computerSelection +  ' beats ' + playerSelection + '.';
+    }
+}
